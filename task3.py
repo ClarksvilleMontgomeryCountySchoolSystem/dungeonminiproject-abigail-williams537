f7a1864b0eed6f77db3bde8c870f196ba24049da
abigail-williams537 <abigail.williams537@students.cmcss.net>
2026-03-05 07:49:17 -0600

    hello

--- task3.py
+++ task3.py
@@ -2,7 +2,7 @@
 guard_alert = False
 
 if not guard_alert:
-    outcome  = "Silent: You slip past the guard unnoticed."
+    outcome  = "Shadow: You slip past the guard unnoticed."
 else:
     outcome = "Doom: The guard spots you and raises the alarm."
 print(outcome)
