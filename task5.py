dd84a56e02ab521716310a4091c7fb7f7a0c2813
abigail-williams537 <abigail.williams537@students.cmcss.net>
2026-03-05 07:47:16 -0600

    hello

---
+++ task5.py
@@ -0,0 +1,8 @@
+# Task 5 - The escape
+escaped = True
+
+if escaped:
+    outcome = "Legend: You step into freedom as the gates fade behind you."
+else:
+    outcome = "Doom: The dungeon claims another last soul."
+print(outcome)
