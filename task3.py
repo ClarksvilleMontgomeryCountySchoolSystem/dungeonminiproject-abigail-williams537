4feeac98a8a6bf770782c56a04c92ca4306070c4
abigail-williams537 <abigail.williams537@students.cmcss.net>
2026-03-05 07:53:40 -0600

    hello

--- task3.py
+++ task3.py
@@ -1,7 +1,7 @@
 # Task 3 - The Guard
-guard_alert = True
+guard_alert = False
 
-if guard_alert:
+if not guard_alert:
     outcome  = "Shadow: You slip past the guard unnoticed."
 else:
     outcome = "Doom: The guard spots you and raises the alarm."
